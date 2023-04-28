1541017b1eb99b9a7e56dc4b4d94d44615080e8f
mericalp <83503845+mericalp@users.noreply.github.com>
2023-04-28 23:25:26 +0300

    Starter

---
+++ SocialMediaApp/SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Meric Alp on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
