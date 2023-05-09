08f5976a0310e5c0922db3c84d5603d8b9b2bb71
Emir <69427895+emirgl@users.noreply.github.com>
2023-05-09 15:29:44 +0300

    Update index.js

--- SocialMedia-API/src/index.js
+++ SocialMedia-API/src/index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
-app.use(postRouter)
 app.use(notiRouter)
+app.use(postRouter)
 
 app.listen(port, () => { 
     console.log('Server is up on port ' + port)
