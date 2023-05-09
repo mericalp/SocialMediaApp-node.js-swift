432f90bbe3c7a135a5671e12e593ca3e804e47a0
Emir <69427895+emirgl@users.noreply.github.com>
2023-05-09 15:30:14 +0300

    swagger configs to index.js

--- SocialMedia-API/src/index.js
+++ SocialMedia-API/src/index.js
@@ -3,16 +3,37 @@ require("./db/mongoose")
 const userRouter = require('./routers/user')
 const postRouter = require('./routers/posts')
 const notiRouter = require('./routers/notifications')
+
+const swaggerUi = require("swagger-ui-express")
+const swaggerJSDoc = require('swagger-jsdoc')
+
 const app = express()
-
-
 const port = process.env.PORT || 3000
 
+const options = {
+    definition: {
+        basepath: '/',
+        openapi: '3.0.0',
+        info : {
+            title: 'Nodejss',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+              url: 'http://localhost:3000',
+            }
+          ]
+    },
+    apis: [`${__dirname}/routers/*.js`]
+}
 
+const swaggerSpec = swaggerJSDoc(options)
+
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(express.json())
 app.use(userRouter)
-app.use(notiRouter)
 app.use(postRouter)
+app.use(notiRouter)
 
 app.listen(port, () => { 
     console.log('Server is up on port ' + port)
