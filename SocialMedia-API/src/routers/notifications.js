26b34fa7a03e83b4cee319805b984220ae363966
Emir <69427895+emirgl@users.noreply.github.com>
2023-05-15 20:16:32 +0300

    notifications post method swagger document

--- SocialMedia-API/src/routers/notifications.js
+++ SocialMedia-API/src/routers/notifications.js
@@ -3,6 +3,64 @@ const auth = require('../middleware/auth')
 const Notification = require('../models/notifications')
 const router = new express.Router()
 
+
+/**
+ * @swagger
+ * /notification:
+ *   post:
+ *     summary: Bildirim oluşturur.
+ *     description: Bildirim oluşturmamızı sağlar.
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: İstek gövdesi.
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string,
+ *             notSenderId:
+ *               type: integer,
+ *               required: true
+ *             notReceiverId:
+ *               type: integer,
+ *             notificationType:
+ *               type: string,
+ *             postText:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Bildirim başarıyla oluşturuldu.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: Bildirim başarıyla oluşturuldu.
+ *             data:
+ *               type: object
+ *               description: Bildirim başarıyla oluşturuldu.
+ *               properties:
+ *                 username:
+ *                    type: string,
+ *                 notSenderId:
+ *                    type: integer,
+ *                    required: true
+ *                 notReceiverId:
+ *                    type: integer
+ *                 notificationType:
+ *                    type: string,
+ *                 postText:
+ *                    type: string
+ *       400:
+ *         description: "Bir Hata Belirdi !"
+ */
+
 router.post('/notification', auth, async (req, res) => {
     const notification = new Notification({
         ...req.body,
